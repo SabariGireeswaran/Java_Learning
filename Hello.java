a0a0187259d04d2ad20a2df3de587d0aa470d3bd
Sabari Gireeswaran B S R <bsrsabarigireeswaran@gmail.com>
2026-05-21 22:56:46 +0530

    feat:add Java files

---
+++ Hello.java
@@ -0,0 +1,16 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        String variable = "7";
+        System.out.println("Hello World");
+        System.out.println("Hello");
+        System.out.println('h');
+        System.out.println(7+"is my Idol's jearsey number");
+        System.out.println(7+" is my Idol's jearsey number");
+        System.out.print("Thala Dhoni" + " " + variable);
+        System.out.print("Thala Dhoni" + " " + variable);
+        System.out.println("Thala Dhoni" + " " + variable);
+        System.out.print("Thala Dhoni" + " " + variable);
+    }
+}
---
+++ variables.java
@@ -0,0 +1,34 @@
+public class variables {
+    public static void main(String args[])
+    {
+        // Integer
+        long a = 6488_464487456646689L;  // We can use l or L 
+        int b = 243_5353;             //for specifying long
+        byte c = 3;
+        short d = 21;
+
+        //Float
+        float e = 33.213_134f;
+        double f = 3.1_4;
+
+        //Character
+        char g = 'h';
+        String h = "d";
+
+        //Boolean
+        boolean i = true;
+
+        //Printing
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c); 
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+     
+    }
+    
+}
